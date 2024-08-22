8e49d58740806f1a634c0c37b5ca15b133abdd52
RebuyBoy <67625553+RebuyBoy@users.noreply.github.com>
2024-08-23 01:43:33 +0700

    init

---
+++ src/main/java/model/GameType.java
@@ -0,0 +1,18 @@
+package model;
+
+
+public enum GameType {
+    HOLDEM_6MAX,
+    HOLDEM_RC,
+    HOLDEM_AOF,
+    OMAHA,
+    OMAHA_FIVE,
+    SPIN_GOLD,
+    SHORT_DECK,
+    FLIP_GO,
+    HOLDEM_9MAX,
+    OMAHA_RC,
+    OMAHA_AOF,
+    BATTLE_ROYALE,
+    SIT_GO_AOF
+}
